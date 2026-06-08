005cfeb378284c18022605b285fe7e9c3ec97784
Hynek Vychodil <vychodil.hynek@gmail.com>
2026-06-08 22:31:32 +0200

    Move expect test beck to p01 implemntation

--- lib/p01_last.ml
+++ lib/p01_last.ml
@@ -5,3 +5,15 @@ let rec last = function
   | [ x ] -> Some x
   | x :: xs -> last xs
 ;;
+
+let%expect_test "P01 test last" =
+  [ "Empty", []; "Single", [ "a" ]; "List", [ "a"; "b"; "c"; "d" ] ]
+  |> List.iter ~f:(fun (n, l) ->
+    printf !"%s: %{sexp:string Option.t}\n" n (last l));
+  [%expect
+    {|
+    Empty: ()
+    Single: (a)
+    List: (d)
+    |}]
+;;
